c36238a4c11afc96860ed04a2a4d27585abcf0f4
Anton-Augustsson <anton-augustsson99@gmail.com>
2025-08-24 13:15:23 +0200

    ltex and keybinding for code action

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code actions' })
 
 require("config.lazy")
 vim.cmd.colorscheme "catppuccin"
